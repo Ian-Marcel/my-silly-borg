3ab1523d4fea33c5ada881c0cc9b4486567486e6
Ian <comercial@ianmarcel.dev>
2026-06-15 03:35:32 -0300

    Add environment variable loading logic to entrypoint.sh: source .env if exists, define default backup user, destination path, and items array

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+# Tells where the script is located
+SHPWD=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+# check if .env file exists
+if [ -e "$SHPWD/.env" -a -f "$SHPWD/.env" ]; then
+    source "$SHPWD/.env"
+fi
+BKP_USER=${BACKUP_USER:-'backup'}             # user that will orchestrate the backup
+BKP_DSTN=${BACKUP_DESTINATION:-'/mnt/backup'} # where the backup will be stored
+BKP_ITMS=()                                   # items that will be archived

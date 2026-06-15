de03460e87e8d326444bf7d8c0423cbdce007982
Ian <comercial@ianmarcel.dev>
2026-06-15 03:37:27 -0300

    Update backup destination path from `/mnt/backup` to `/mnt/backup/data` and add log storage location at `/mnt/backup/log` in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ SHPWD=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 if [ -e "$SHPWD/.env" -a -f "$SHPWD/.env" ]; then
     source "$SHPWD/.env"
 fi
-BKP_USER=${BACKUP_USER:-'backup'}             # user that will orchestrate the backup
-BKP_DSTN=${BACKUP_DESTINATION:-'/mnt/backup'} # where the backup will be stored
-BKP_ITMS=()                                   # items that will be archived
+BKP_USER=${BACKUP_USER:-'backup'}                    # user that will orchestrate the backup
+BKP_DSTN=${BACKUP_DESTINATION:-'/mnt/backup/data'}   # where the backup will be stored
+BKP_LOG_STG=${BACKUP_LOG_STORAGE:-'/mnt/backup/log'} # where the backup's logs will be stored
+BKP_ITMS=()                                          # items that will be archived
